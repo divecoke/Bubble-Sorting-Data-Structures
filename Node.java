0b86cd8f98c6931c192ebf999428a6075cef7158
Maggie <meetmagdalene@users.noreply.github.com>
2014-02-19 19:23:38 -0600

    Update Node.java
    adjusted comments and Javadocs for readability.

--- Node.java
+++ Node.java
@@ -1,42 +1,36 @@
 /**
-* @author 	Magdalene Benson <mebenson12@winona.edu>
-* @version 	1.1
-* @since	01/18/2014
-*
-* @description	A class that defines an int Node.
-*
+*  @Author 	Magdalene Benson <thecodingbarista@gmail.com>
+*  Date: 1	2/12/2013  
+*  Course: 	WSU-R CS341 Data Structures
+*  Assignment: Assignment 3 - Sorting Algorithms (Node.java)
+*  
+*  Description: Implements a Node to be used in a Singly-Linked List Data Structure.
 */
 
 /**
- * Class: 		Node
- * Contains:	Node() 				// Default Constructor
- * 				Node(param) 		// User-Defined Constructor
- * 				Node(param, param) 	// User-Defined Constructor
- * 				setNext()
- * 				setInt()
- * 				getNext()
- * 				getInt()
+ * Class: 	Node
+ * Contains:	Node() 			// Default Constructor
+ * 		Node(param) 		// User-Defined Constructor
+ * 		Node(param, param) 	// User-Defined Constructor
+ * 		setNext()
+ * 		setInt()
+ * 		getNext()
+ * 		getInt()
  * Function:	Creates a new Node.
  */
 public class Node {
 	
-	/**
-	 * An int value to add to the Node.
-	 */
+	// Instance Variables
 	private int myInt;
-	
-	/**
-	 * A Node that points to the next element/Node.
-	 */
 	private Node next;
 	
 	/**
 	 * Constructor:		Node::Node()
 	 * 
 	 * Function:		A constructor that initializes a single Node.
-	 * 					No key value assigned.
+	 * 			No key value assigned.
 	 * 
-	 * @param 			None
+	 * @param 		None
 	 */
 	public Node() {
 		myInt = 0;
@@ -48,7 +42,7 @@ public class Node {
 	 * 
 	 * Function:		A constructor that initializes a Node with a key value.
 	 * 
-	 * @param 			myInt	The key value to assign to the Node value.
+	 * @param 		myInt	The key value to assign to the Node value.
 	 */
 	public Node(int myInt) {
 		this.myInt = myInt;
@@ -59,10 +53,10 @@ public class Node {
 	 * Constructor:		Node::Node(param, param)
 	 * 
 	 * Function:		A constructor that initializes a Node with a key value and assigns
-	 * 					the pointer to the 'next' Node.
+	 * 			the pointer to the 'next' Node.
 	 * 
-	 * @param 			myInt	The key value to assign to the Node value.
-	 * @param 			next	The Node to point to 'next'.
+	 * @param 		myInt	The key value to assign to the Node value.
+	 * @param 		next	The Node to point to 'next'.
 	 */
 	public Node(int myInt, Node next) {
 		this.myInt = myInt;
@@ -70,47 +64,48 @@ public class Node {
 	}
 	
 	/**
-	 * Method:		Node::setNext()
+	 * Method:	Node::setNext()
 	 * 
 	 * Function:	Sets the pointer of this Node to the next Node.
 	 * 
-	 * @param 		next The Node to point to next.
+	 * @param 	next The Node to point to next.
 	 */
 	public void setNext(Node next) {
 		this.next = next;
 	}
 	/**
-	 * Method:		Node::setInt()
+	 * Method:	Node::setInt()
 	 * 
 	 * Function:	Sets the key value of this Node.
 	 * 
-	 * @param 		myInt	The key value to insert in the Node.
+	 * @param 	myInt	The key value to insert in the Node.
 	 */
 	public void setInt(int myInt) {
 		this.myInt = myInt;
 	}
 	
 	/**
-	 * Method:		Node::getNext()
+	 * Method:	Node::getNext()
 	 * 
 	 * Function:	Returns the Node this Node is pointing to.
 	 * 
-	 * @param		None
-	 * @return 		Returns the pointer to the next Node.
+	 * @param	None
+	 * @return 	Returns the pointer to the next Node.
 	 */
 	public Node getNext() {
 		return next;
 	}
 	
 	/**
-	 * Method:		Node:: getInt()
+	 * Method:	Node:: getInt()
 	 * 
 	 * Function:	Gets the key value for this Node.
 	 * 
-	 * @param		None
-	 * @return 		Returns the key value of this Node.
+	 * @param	None
+	 * @return 	Returns the key value of this Node.
 	 */
 	public int getInt() {
 		return myInt;
 	}
-}
+	
+} // Ends Node Class
